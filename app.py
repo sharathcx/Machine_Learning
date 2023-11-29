c74595714e3721bde6913743f4fb63557d126206
Iamsharath007 <sharathpournami@gmail.com>
2023-11-29 22:02:10 +0530

    app.py updated

--- app.py
+++ app.py
@@ -11,10 +11,12 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=['*']
 )
+obj1 = GradientDecent()
 
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File()):
+    global obj1
     path = "C:/Users/shara/OneDrive/Desktop/Machine Learning/data.xlsx"
     with open(path, "wb") as data:
         data.write(file.file.read())
@@ -22,11 +24,22 @@ async def upload_file(file: UploadFile = File()):
     df = pd.read_excel(path)
     x_train = np.array(df.iloc[:, 1].values)
     y_trian = np.array(df.iloc[:, 2].values)
-    obj1 = GradientDecent(x_train, y_trian)
+    obj1.x_train = x_train
+    obj1.y_train = y_trian
     obj1.plotter(path="C:/Users/shara/OneDrive/Desktop/Machine Learning/plot")
+    return obj1.gradient_descent()
+
 
 @app.get("/get_file")
 async def get_file():
     path = "C:/Users/shara/OneDrive/Desktop/Machine Learning/plot.png"
     return FileResponse(path, filename="plot.png")
+
+
+@app.get("/predict")
+async def predict(x: float):
+    x_mean = obj1.x_train.mean()
+    w, b, = obj1.gradient_descent()
+    print(obj1.x_train)
+    return obj1.predict(w, b, x)
 
